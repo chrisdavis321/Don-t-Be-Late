b932f028bd96baf3d3886c2158003b28946d6770
chrisdavis321 <cdavi114@bulldogs.aamu.edu>
2023-03-21 10:37:03 -0500

    SideMenu
    In progress still

---
+++ SeniorProbApp/SideMenuViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SideMenuViewController.swift
+//  SeniorProbApp
+//
+//  Created by Christopher Davis on 3/21/23.
+//
+
+import SwiftUI
+
+struct SideMenuViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SideMenuViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        SideMenuViewController()
+    }
+}
